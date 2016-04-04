a334de8459e9ddcd7805a6a8c485f8cb61d5a66c
mvartani76 <mike.vartanian@gmail.com>
2016-04-04 13:09:22 -0400

    Updated the $breakpoints array to include the breakpoints from P2 but with foundation-ish type names. Also updated index to use interchange instead of picturefill with the updated breakpoints.

--- index.php
+++ index.php
@@ -17,13 +17,15 @@
   <body>
 
 	<div class="wrapper">
-		<div class="row">
-			<header class="large-12 columns">
-			<img data-interchange="[img/sog-entrance-800.jpg, small], 
-				[img/sog-entrance-1000.jpg, medium], 
-				[img/sog-entrance-1200.jpg, large]">
+
+		<!-- Replaced header with row div as this seems to allow the header image to stay
+			 adjacent to the menu when scaling to the different screen sizes. -->
+		<div class=" row xlarge-12 large-12 medium-12 columns">
+			<img data-interchange="[img/sog-entrance-500.jpg, small], 
+				[img/sog-entrance-800.jpg, medium], 
+				[img/sog-entrance-1000.jpg, large], 
+				[img/sog-entrance-1200.jpg, xlarge]">
 			<noscript><img src="img/sog-entrance-800.jpg.jpg"></noscript>
-			</header>
 		</div>
 
 		<div class="title-bar" data-responsive-toggle="nav-menu" data-hide-for="medium">
@@ -44,33 +46,35 @@
 			</div>
 		</div>
 		<div class="row">
-			<main class="large-9 columns">
+			<main class="xlarge-9 large-9 medium-9 columns">
 				<h2>Welcome to the Shores of Glenwood Neighborhood Association Website!</h2>
 				
-				<!-- Using picturefill, load different pictures depending on the width of the screen
+				<!-- Using interchange, load different pictures depending on the width of the screen
 				This picture is in the upper left hand corner of the main section under the <h2> title... -->
 				<div class="first_main_pic">
 					<img data-interchange="[img/sog-entrance-drive-orig-350.jpg, small], 
-						[img/sog-entrance-drive-orig-250.jpg, medium], 
-						[img/sog-entrance-drive-orig-350.jpg, large]">
+						[img/sog-entrance-drive-orig-200.jpg, medium], 
+						[img/sog-entrance-drive-orig-250.jpg, large], 
+						[img/sog-entrance-drive-orig-350.jpg, xlarge]">
 					<noscript><img src="img/sog-entrance-drive-orig-350.jpg"></noscript>
 				</div>
 
 				<p>This site contains information about our great community located in Commerce Township, Michigan. Shores of Glenwood is a highly esteemed neighborhood of homes set in a carefully preserved environment with majestic oak trees and evergreens, beautiful rolling hills, a few ponds, a gently flowing stream, and lots of natural wildlife.</p>
 				
-				<!-- Using picturefill, load different pictures depending on the width of the screen
+				<!-- Using interchange, load different pictures depending on the width of the screen
 				This picture is in the lower right hand corner of the main section -->
 				<div class="second_main_pic">
-					<img data-interchange="[img/sog-entrance-sign-150.jpg, small], 
-						[img/sog-entrance-sign-200.jpg, medium], 
-						[img/sog-entrance-sign-250.jpg, large]">
+					<img data-interchange="[img/sog-entrance-sign.jpg, small], 
+						[img/sog-entrance-sign-150.jpg, medium],
+						[img/sog-entrance-sign-200.jpg, large],
+						[img/sog-entrance-sign-250.jpg, xlarge]">
 					<noscript><img src="img/sog-entrance-sign.jpg"></noscript>					
 				</div>
 
 				<p>We are an active community of families, young couples, empty-nesters, and other lively people who enjoy a peaceful atmosphere and close proximity to city attractions and water recreation areas.</p>
 
 			</main>
-			<div class="outside-w-wrapper large-3 columns">
+			<div class="outside-w-wrapper xlarge-3 large-3 medium-3 columns">
 				<h3>Local Time &amp; Weather</h3>
 				<p>
 					<?php
@@ -122,7 +126,7 @@
 		</div>
 	</div>
 	<div class="row">
-	    <footer class="large-12 columns">
+	    <footer class="xlarge-12 large-12 columns">
 	    	<p>&copy; 2016 Shores of Glenwood Neighborhood Association </p>
 	    </footer>
 	</div>
