0999e9e584c84fc05797f49e2cd1c0818acc184b
mvartani76 <mike.vartanian@gmail.com>
2016-04-04 15:12:28 -0400

    Updated header image section and menu. Menu now has orangeish color background as in P2. Updated the columns for main, weather wrapper, and the footer. This file currently uses the old style sheets which I will need to incorporate into the foundation setup.

--- thingstodo.php
+++ thingstodo.php
@@ -9,17 +9,22 @@
 	<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/foundation.css" />
     <link rel="stylesheet" href="css/app.css" />
-  	<script type="text/javascript" src="js/menu.js"></script>
+	<link rel="stylesheet" href="css/styles.css">    
+	<link rel="stylesheet" href="css/weather-style.css">
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   </head>
   <body>
 	<div class="wrapper">
-		<div class="row">
-			<header class="large-12 columns">
-				<img data-interchange="[img/sog-entrance-800.jpg, small], 
-					[img/sog-entrance-1000.jpg, medium], 
-					[img/sog-entrance-1200.jpg, large]">
-				<noscript><img src="img/sog-entrance-800.jpg.jpg"></noscript>
-			</header>
+
+		<!-- Replaced header with row div as this seems to allow the header image to stay
+			 adjacent to the menu when scaling to the different screen sizes. -->
+		<div class=" row xlarge-12 large-12 medium-12 columns">
+			<img data-interchange="[img/sog-entrance-500.jpg, small], 
+				[img/sog-entrance-800.jpg, medium], 
+				[img/sog-entrance-1000.jpg, large], 
+				[img/sog-entrance-1200.jpg, xlarge]">
+			<noscript><img src="img/sog-entrance-800.jpg.jpg"></noscript>
 		</div>
 
 		<div class="title-bar" data-responsive-toggle="nav-menu" data-hide-for="medium">
@@ -28,28 +33,23 @@
 		</div>
 
 		<div class="row">
-			<!--<div class="menu large-12 columns">-->
-			<div class="top bar" id="nav-menu">
-				<div class="top-bar-left">
-					<!--<ul class="active-toggle">-->
-					<ul class="menu">
-						<li><a href="./index.php">Home</a></li>
-						<li><a href="#">About</a></li>
-						<li class="active"><a href="./thingstodo.php">Things to Do</a></li>
-						<li><a href="./photogallery.php">Photo Gallery</a></li>
-						<li><a href="./login.php">Login</a></li>
-						<li><a href="./register.php">Register</a></li>
-						<li><a href="./contact.php">Contact Us</a></li>
-					</ul>
-					<!--<a class="toggle-nav" href="#">&#9776;</a>-->
-				</div>
+			<div class="top-bar" id="nav-menu">
+				<ul class="menu expanded">
+					<li><a href="./index.php">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li class="active"><a href="./thingstodo.php">Things to Do</a></li>
+					<li><a href="./photogallery.php">Photo Gallery</a></li>
+					<li><a href="./login.php">Login</a></li>
+					<li><a href="./register.php">Register</a></li>
+					<li><a href="./contact.php">Contact Us</a></li>
+				</ul>
 			</div>
 		</div>
 		<div class="row" data-equalizer>
 			<!-- Added the col-push-1 class which moves this column right 25% to make this display (col-2)
 			     display in the middle for screen sizes >479px.
 			     For screen sizes <=479px, they will display in normal order as written. -->
-			<main class="large-6 large-push-3 columns" data-equalizer-watch>
+			<main class="xlarge-6 large-6 medium-6 xlarge-push-3 large-push-3 medium-push-3 columns" data-equalizer-watch>
 				<h2>Things to Do in Commerce Township</h2>
 				
 				<p style="clear: both;">As there are many lakes and rivers nearby, there are many water activities to do here in our community from boating and water skiing to
@@ -152,7 +152,7 @@
 			<!-- Added the col-push-1 class which moves this column right 25% to make this display (col-1)
 			     display in the right for screen sizes >479px
 			     For screen sizes <=479px, they will display in normal order as written. -->
-			<div class="large-3 large-push-3 outside-w-wrapper" data-equalizer-watch>
+			<div class="xlarge-3 large-3 medium-3 xlarge-push-3 large-push-3 medium-push-3 columns outside-w-wrapper" data-equalizer-watch>
 				<h3>Local Time &amp; Weather</h3>
 				<p>
 					<?php
@@ -205,7 +205,7 @@
 			<!-- Added the col-pull-3 class which moves this column left 75% to make this display (col-1)
 			     display in the left for screen sizes >479px
 			     For screen sizes <=479px, they will display in normal order as written. -->
-			<aside class="large-3 large-pull-9" data-equalizer-watch>
+			<aside class="xlarge-3 large-3 medium-3 xlarge-pull-9 large-pull-9 medium-pull-9 columns" data-equalizer-watch>
 				<h3>Commerce Township Info</h3>
 				<ul>
 					<li><a href="https://en.wikipedia.org/wiki/Commerce_Township,_Michigan" target="_blank">Population:</a> 40,186</li>
@@ -223,7 +223,7 @@
 		</div>
 	</div>
 	<div class="row">
-	    <footer class="large-12 columns">
+	    <footer class="xlarge-12 large-12 medium-12 columns">
 	    	<p>&copy; 2016 Shores of Glenwood Neighborhood Association </p>
 	    </footer>
 	</div>
@@ -262,17 +262,6 @@
 			
 		});
 	
-	</script>
-	<script src="js/eqheight.js"></script>
-	<script type="text/javascript">
-	if ('addEventListener' in window) {
-	    window.addEventListener('resize', function(){
-	        sameHeights();
-	    });
-	    window.addEventListener('load', function(){
-	        sameHeights();
-	    });
-	}
 	</script>
     <script src="js/vendor/jquery.min.js"></script>
     <script src="js/vendor/what-input.min.js"></script>
